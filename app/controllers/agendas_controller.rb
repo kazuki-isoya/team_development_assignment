062c163e179d11e838c09119c544966d70e2eb4e
moru more <moremoru.world21@gmail.com>
2020-03-17 22:15:18 +0900

    アジェンダコントローラーに削除機能を追加

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -1,5 +1,6 @@
 class AgendasController < ApplicationController
   # before_action :set_agenda, only: %i[show edit update destroy]
+  before_action :set_agenda, only: [:destroy]
 
   def index
     @agendas = Agenda.all
@@ -15,9 +16,22 @@ class AgendasController < ApplicationController
     @agenda.team = Team.friendly.find(params[:team_id])
     current_user.keep_team_id = @agenda.team.id
     if current_user.save && @agenda.save
-      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda') 
+      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda')
     else
       render :new
+    end
+  end
+
+#アジェンダの削除機能を追記
+  def destroy
+    if @agenda.team.owner_id == current_user.id || @agenda.user_id == current_user.id
+      @agenda.team.members.each do |member|
+        DestroyAgendaMailer.destroy_agenda_mail(@agenda, member).deliver
+      end
+      @agenda.destroy
+        redirect_to dashboard_url, notice: "アジェンダ「#{@agenda}」を削除しました。"
+      else
+        redirect_to :back, notice: "アジェンダ「#{@agenda}」の削除に失敗しました。"
     end
   end
 
